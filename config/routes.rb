0903ec9f254280730d8b363efc67303febdecd4d
Jessica Ribeiro <jessica.ribeiro@hotmail.fr>
2022-05-19 16:59:18 +0200

    API done for the seed file and app working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists
+
+  root to: 'lists#index'
+
+  resources :lists, only: %i[show create new index] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
